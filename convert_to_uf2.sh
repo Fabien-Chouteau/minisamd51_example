e4cde3f9085440ccb0fe5f82264a1ab327798332
Fabien Chouteau <chouteau@adacore.com>
2021-01-12 17:58:53 +0100

    Improve the uf2 convert script
    With Alire the location of the binary is not in obj/.

--- convert_to_uf2.sh
+++ convert_to_uf2.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-arm-eabi-objcopy -O binary obj/minisamd51_example.elf obj/minisamd51_example.bin
+
+
+arm-eabi-objcopy -O binary $(find . -name minisamd51_example.elf) minisamd51_example.bin
 
 wget https://raw.githubusercontent.com/microsoft/uf2/master/utils/uf2conv.py
 
-python2 uf2conv.py -b 16384 -c -o minisamd51_example.uf2 obj/minisamd51_example.bin
+python2 uf2conv.py -b 16384 -c -o minisamd51_example.uf2 minisamd51_example.bin
